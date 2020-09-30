6da1eab62bb91cec00d670f0041ea5b915f4d185
Yuriy Bachevskiy <Materik-r@yandex.ru>
2020-09-30 14:58:20 +0700

    formatting code according to phpcs

--- index.php
+++ index.php
@@ -1,4 +1,17 @@
 <?php
+/**
+ * MyClass doc comment
+ *
+ * Long description Long description Long description Long description
+ *
+ * php version 7.4.3
+ *
+ * @category Category
+ * @package  Package
+ * @author   author <mail@mail.ru>
+ * @license  GNU gnu.org
+ * @link     somelink
+ */
 
 require __DIR__ . '/vendor/autoload.php';
 
--- src/Runner.php
+++ src/Runner.php
@@ -1,14 +1,36 @@
 <?php
 
+/**
+ * MyClass doc comment
+ *
+ * Long description Long description Long description Long description
+ *
+ * php version 7.4.3
+ *
+ * @category Category
+ * @package  Package
+ * @author   author <mail@mail.ru>
+ * @license  GNU gnu.org
+ * @link     somelink
+ */
 namespace Hexlet\Php\Runner;
 
 use Tightenco\Support\Collection;
 
+/**
+ * Some comment
+ * 
+ * @return \Tightenco\Collect\Support\Collection
+ */
 function run()
 {
-    $collection = collect(['taylor', 'abigail', null])->map(function ($name) {
-        return strtoupper($name);
-    });
+    $collection = collect(
+        ['taylor', 'abigail', null]
+    )->map(
+        function ($name) {
+            return strtoupper($name);
+        }
+    );
 
     return $collection;
 }
